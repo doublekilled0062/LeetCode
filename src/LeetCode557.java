60f6ca9209f1e1007bed2444186a086df90c998a
mengxiangyu <mengxiangyu1987@126.com>
2019-03-25 19:58:20 +0800

    反转字符串

---
+++ src/LeetCode557.java
@@ -0,0 +1,41 @@
+/**
+ * 557. 反转字符串中的单词 III
+ *
+ * 给定一个字符串，你需要反转字符串中每个单词的字符顺序，同时仍保留空格和单词的初始顺序。
+ * 示例 1:
+ * 输入: "Let's take LeetCode contest"
+ * 输出: "s'teL ekat edoCteeL tsetnoc"
+ * 注意：在字符串中，每个单词由单个空格分隔，并且字符串中不会有任何额外的空格。
+ */
+public class LeetCode557 {
+    /**
+     * 遍历找到空格或者到头就交换
+     * @param s
+     * @return
+     */
+    public String reverseWords(String s) {
+        char[] chars = s.toCharArray();
+        int i = 0;
+        int j = 0;
+        int jLast = 0;
+        while (j < chars.length){
+            for(; j < chars.length; j++){
+                if(j == chars.length - 1 || chars[j + 1] == ' '){
+                    jLast = j + 2;
+                    break;
+                }
+            }
+            //跳出循环反转单词
+            while (i < j){
+                char temp = chars[i];
+                chars[i] = chars[j];
+                chars[j] = temp;
+                i++;
+                j--;
+            }
+            i = jLast;
+            j = jLast;
+        }
+        return new String(chars);
+    }
+}
