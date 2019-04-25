047cc424ed2cae5c54bd0795039ed1b09bec372b
mengxiangyu <mengxiangyu1987@126.com>
2019-04-26 00:11:06 +0800

    二叉树向右节点 非递归优化 递归写法

--- src/LeetCode117.java
+++ src/LeetCode117.java
@@ -31,77 +31,107 @@
  * 使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
  */
 public class LeetCode117 {
+    /**
+     * 非递归常量空间解法
+     * 比递归解法慢在后面
+     * @param root
+     * @return
+     */
     public Node connect(Node root) {
         if(root == null){
             return root;
         }
         Node levelNode = root;
+        //为了优化底下注释 引入一个临时变量 在下面循环的时候 就找到了levelNode的下一个
+        Node nextLevelNode = null;
         while (levelNode != null){
             Node cur = levelNode;
             while (cur != null){
+                //节省点代码的判断 要先判断左节点
                 if(cur.left != null){
-                    //有右节点
+                    if(nextLevelNode == null){
+                        nextLevelNode = cur.left;
+                    }
                     if(cur.right != null){
-                        //左到右
+                        //左向右
                         cur.left.next = cur.right;
-                        //有next
-                        Node next = cur.next;
-                        while (next != null){
-                            //next左不空就指左 右不空就指右 全空就不指
-                            if(next.left != null){
-                                cur.right.next = next.left;
-                                break;
-                            }else if(next.right != null){
-                                cur.right.next = next.right;
-                                break;
-                            }else {
-                                next = next.next;
-                            }
-                        }
                     }else {
-                        Node next = cur.next;
-                        while (next != null){
-                            //next左不空就指左 右不空就指右 全空就不指
-                            if(next.left != null){
-                                cur.left.next = next.left;
-                                break;
-                            }else if(next.right != null){
-                                cur.left.next = next.right;
-                                break;
-                            }else {
-                                next = next.next;
-                            }
-                        }
+                        //左向 下一个
+                        cur.left.next = nextNode(cur.next);
                     }
-                }else {
-                    //只有右节点
-                    if(cur.right != null){
-                        //有next
-                        Node next = cur.next;
-                        while (next != null){
-                            //next左不空就指左 右不空就指右 全空就不指
-                            if(next.left != null){
-                                cur.right.next = next.left;
-                                break;
-                            }else if(next.right != null){
-                                cur.right.next = next.right;
-                                break;
-                            }else {
-                                next = next.next;
-                            }
-                        }
+                }
+                if(cur.right != null){
+                    if(nextLevelNode == null){
+                        nextLevelNode = cur.right;
                     }
+                    //右向下一个
+                    cur.right.next = nextNode(cur.next);
                 }
                 cur = cur.next;
             }
-            if(levelNode.left != null){
-                levelNode = levelNode.left;
-            }else if(levelNode.right != null){
-                levelNode = levelNode.right;
-            }else {
-                levelNode = levelNode.next;
+            //慢在这里 可能会一直遍历完最后一层
+//            if(levelNode.left != null){
+//                levelNode = levelNode.left;
+//            }else if(levelNode.right != null){
+//                levelNode = levelNode.right;
+//            }else {
+//                levelNode = levelNode.next;
+//            }
+            levelNode = nextLevelNode;
+            nextLevelNode = null;
+        }
+        return root;
+    }
+
+    private Node nextNode(Node node) {
+        while (node != null) {
+            if (node.left != null) {
+                return node.left;
+            }
+            if (node.right != null) {
+                return node.right;
+            }
+            node = node.next;
+        }
+        return null;
+    }
+
+    /**
+     * 网友递归写法放这里了 和上面非递归其实差不多
+     * @param root
+     * @return
+     */
+    public Node connect2(Node root) {
+        if (root == null) {
+            return null;
+        }
+        if (root.left != null) {
+            if (root.right != null) {
+                // 若右子树不为空，则左子树的 next 即为右子树
+                root.left.next = root.right;
+            } else {
+                // 若右子树为空，则右子树的 next 由根节点的 next 得出
+                root.left.next = nextNode(root.next);
             }
         }
+        if (root.right != null) {
+            // 右子树的 next 由根节点的 next 得出
+            root.right.next = nextNode(root.next);
+        }
+        // 先确保 root.right 下的节点的已完全连接，因 root.left 下的节点的连接
+        // 需要 root.left.next 下的节点的信息，若 root.right 下的节点未完全连
+        // 接（即先对 root.left 递归），则 root.left.next 下的信息链不完整，将
+        // 返回错误的信息。可能出现的错误情况如下图所示。此时，底层最左边节点将无
+        // 法获得正确的 next 信息：先递归虚线
+        //                  o root
+        //                 / \
+        //     root.left  o —— o  root.right
+        //               /    / \
+        //              o —— o-- o
+        //             /        / \
+        //            o        o   o
+        connect2(root.right);
+        connect2(root.left);
         return root;
     }
 
@@ -119,5 +149,28 @@ public class LeetCode117 {
             right = _right;
             next = _next;
         }
-    };
+    }
+
+    public static void main(String[] args) {
+        LeetCode117 leetCode117 = new LeetCode117();
+        Node node1 = leetCode117.new Node();
+        Node node2 = leetCode117.new Node();
+        Node node3 = leetCode117.new Node();
+        Node node4 = leetCode117.new Node();
+        Node node5 = leetCode117.new Node();
+        Node node7 = leetCode117.new Node();
+        node1.val = 1;
+        node2.val = 2;
+        node3.val = 3;
+        node4.val = 4;
+        node5.val = 5;
+        node7.val = 7;
+
+        node1.left = node2;
+        node1.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+        node3.right = node7;
+        leetCode117.connect(node1);
+    }
 }
