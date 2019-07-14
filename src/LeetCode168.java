6e0303e0d85b80517be65f2b4f205d549fadcdcc
mengxiangyu <mengxiangyu1987@126.com>
2019-07-14 12:59:55 +0800

    excel列名称

---
+++ src/LeetCode168.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 168. Excel表列名称
+ *
+ * 给定一个正整数，返回它在 Excel 表中相对应的列名称。
+ *
+ * 例如，
+ *
+ *     1 -> A
+ *     2 -> B
+ *     3 -> C
+ *     ...
+ *     26 -> Z
+ *     27 -> AA
+ *     28 -> AB
+ *     ...
+ * 示例 1:
+ *
+ * 输入: 1
+ * 输出: "A"
+ * 示例 2:
+ *
+ * 输入: 28
+ * 输出: "AB"
+ * 示例 3:
+ *
+ * 输入: 701
+ * 输出: "ZY"
+ *
+ */
+public class LeetCode168 {
+    public String convertToTitle(int n) {
+        StringBuilder builder = new StringBuilder();
+        while (n != 0){
+            builder.append(((char)((n - 1) % 26 + 'A')));
+            n = (n-1)/26;
+        }
+        return builder.reverse().toString();
+    }
+}
